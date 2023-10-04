ba4e458658be07279e8ba72d7dcb46ecc5eeee1f
BrownCarter <lilbrown3000@gmail.com>
2023-10-04 16:54:59 +0100

    init

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+def canUnlockAll(boxes):
+    keys = set()
+    keys.add(0)
+    stack = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key not in keys:
+                keys.add(key)
+                stack.append(key)
+    return len(keys) == len(boxes)
